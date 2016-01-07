d9e230ff739538bf05c17d158ebfc906f7e2d131
Ana Gabriela Venegas Li <agvenegas@mobtion.com>
2016-01-07 08:49:33 -0600

    new style for the spinner
    new style for the spinner

--- app/src/main/java/com/example/agvenegas/prueba4/NewUserFragment.java
+++ app/src/main/java/com/example/agvenegas/prueba4/NewUserFragment.java
@@ -1,6 +1,5 @@
 package com.example.agvenegas.prueba4;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -9,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
